40e705087b9daa934cc29b889ade929fa8266a5f
arscabar <de08112@naver.com>
2026-07-11 13:25:37 +0900

    Improve in-game text legibility

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import "./styles.css";
 import "./gameplay-responsive.css";
 import "./design-polish.css";
 import "./immersive-design.css";
+import "./gameplay-legibility.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
